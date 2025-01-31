5978037ab3a516062c8fdcd66e3bbb872b624998
Arthur Moreira <arthur_moreira.10@hotmail.com>
2025-01-30 22:15:35 -0300

    Organiza o header e adiciona as novas funções e a matriz de números
    As bibliotecas foram adicionadas no header, a matriz de numeros foi inclusa e as novas funções de led

--- matrix_leds.h
+++ matrix_leds.h
@@ -1,8 +1,23 @@
 #ifndef MATRIX_LEDS_H
 #define MATRIX_LEDS_H
 
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "pio_matrix.pio.h"
+
+extern uint sm;
+
+void init_led(uint8_t pin);
+
+void muda_estado(uint8_t pin);
+
 uint8_t obter_index(uint8_t i);
 
-void desenha_frame(uint32_t matriz[10][25],uint8_t frame,uint sm);
+void desenha_frame(const uint32_t matriz[10][25],uint8_t frame);
 
+void setup_led_matrix();
+
+extern const uint32_t numeros[10][25];
 #endif
